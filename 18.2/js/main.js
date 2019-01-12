37bfdaf9a5c55a239396f524e1a7baecacad3fc7
karma <karma@foxmail.com>
2019-01-12 15:41:46 +0800

    v-for

---
+++ 18.2/js/main.js
@@ -0,0 +1,23 @@
+var app = new Vue({
+    el: '#app',
+    data:{
+        foodList: [
+            {
+                name: '葱',
+                price: 10,
+                discount: 0.8
+            },
+            {
+                name: '姜',
+                price: '8',
+                discount: 0.6
+            },
+            {
+                name: '蒜',
+                price: '5',
+                discount: 0.5
+            }
+        ]
+    }
+
+});
