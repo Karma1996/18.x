2e833652925cdd89bd67107a9a51545d559668ce
karma <karma@foxmail.com>
2019-01-12 20:40:57 +0800

    v-bind

---
+++ 18.3/js/main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        url: 'http://www.baidu.com',
+        img: 'https://dummyimage.com/256x256/ffcc00/ffffff',
+        klass: 'btn btn-default',
+        isActive: true
+    }
+});
