166b9afb29ca7aba43ae0d47cee2c7e9b6c5d7fb
karma <karma@foxmail.com>
2019-01-12 22:54:48 +0800

    radio、checkbox、textarea、select[multiple]

--- 18.6/js/main.js
+++ 18.6/js/main.js
@@ -1,6 +1,10 @@
 var app = new Vue({
     el: '#app',
     data: {
-        sex: []
+        sex: [],
+        article: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Beatae blanditiis eos fugit id inventore iure, laborum laudantium magnam minima non nostrum nulla perspiciatis quasi quibusdam repellendus temporibus tenetur totam voluptatem.`,
+        form: 1,
+        dest: 2
     }
 });
